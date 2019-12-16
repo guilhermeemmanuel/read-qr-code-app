43355dd25b0a6bd4f72bfbaa6b0c223e9d1350f4
Guilherme Neves <guilherme.neves@virtus.ufcg.edu.br>
2019-12-16 16:17:07 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'qr-code-reader-poc';
+  readEnable = false;
+  lastDetection = 0;
+
+  TIME_TO_DETECT = 3000;
+  TIME_TO_SHOW_SNACK = 2000;
+
+  constructor(private snackBar: MatSnackBar) {
+  }
+
+  private startCamera(): void {
+    this.readEnable = true;
+  }
+
+  private stopCamera(): void {
+    this.readEnable = false;
+  }
+
+  private camerasFoundHandler(event): void {
+    console.log(event)
+    console.log('Camera Found');
+  }
+
+  private camerasNotFoundHandler(event: Event): void {
+    console.log('Camera Not Found');
+  }
+
+  private scanSuccessHandler(message): void {
+    const timestamp = new Date().getTime()
+    if (timestamp - this.lastDetection > this.TIME_TO_DETECT) {
+      this.lastDetection = timestamp;
+      this.snackBar.open('QR Code message: ' + message, 'OK!', {
+        duration: this.TIME_TO_SHOW_SNACK,
+      });
+    }
+  }
+
+  private scanErrorHandler(event): void {
+    this.readEnable = false;
+    this.snackBar.open('Erro de leitura detectado. Posicione melhor o QR Code e tente novamente!', 'OK!', {
+      duration: this.TIME_TO_SHOW_SNACK,
+    });
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ZXingScannerModule} from '@zxing/ngx-scanner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSnackBarModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ZXingScannerModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatIconModule,
+    MatSnackBarModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
